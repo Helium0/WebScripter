9e766cdf901903cec46c9a6e5ee89448043183e2
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-21 23:42:21 +0100

    Created methods, class fields, and constructor for PDF file

--- src/test/java/PdfCreator.java
+++ src/test/java/PdfCreator.java
@@ -1,25 +1,37 @@
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
-
 import java.io.FileNotFoundException;
 
 
-public class PdfCreator {
-    public static void main(String[] args) throws FileNotFoundException {
+
+public class PdfCreator  {
+
+    private PdfDocument pdfDocument;
+
+    private Document document;
+
+    private final String MY_PATH = System.getProperty("user.dir") + "\\src\\test\\resources\\scrappedItems.pdf";
 
 
-        String myPath = System.getProperty("user.dir") + "\\src\\test\\resources\\myPdf.pdf";
-
-        PdfDocument pdf = new PdfDocument(new PdfWriter(myPath));
-        Document document = new Document(pdf);
-        String myLine = "TEST";
-        String aa = "okok";
-        document.add(new Paragraph(myLine));
-        document.add(new Paragraph(aa));
-        document.close();
-
-        System.out.println("created file!");
-
+    public PdfCreator() throws FileNotFoundException {
+        this.pdfDocument = new PdfDocument(new PdfWriter(MY_PATH));
+        this.document = new Document(pdfDocument);
     }
+
+
+    public  Document getDocument() {
+        return document;
+    }
+
+
+
+    public void closeDocument() {
+        if(document != null) {
+            document.close();
+        }
+    }
+
+
+
+
 }
