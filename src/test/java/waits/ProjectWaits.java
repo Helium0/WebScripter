66539005437166832436b17df63fbcc90c906d61
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-23 23:57:02 +0100

    Added class with waits methods

---
+++ src/test/java/waits/ProjectWaits.java
@@ -0,0 +1,23 @@
+package waits;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ByIdOrName;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ProjectWaits {
+
+
+    public static WebDriverWait wait(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public static WebElement waitForPresenceElementLocated(WebDriver driver, By locator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+}
