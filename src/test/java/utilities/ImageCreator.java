d7bec8a872cc14a87bbe104123ea8b221c628c19
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-28 10:26:00 +0100

    Validator Image method

--- src/test/java/utilities/ImageCreator.java
+++ src/test/java/utilities/ImageCreator.java
@@ -4,6 +4,7 @@ import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.layout.element.Image;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -19,5 +20,20 @@ public class ImageCreator {
     public static Image convertStrigInToImage(String text) throws MalformedURLException {
         return new Image(createImageStringData(text));
     }
+
+    public static Image validatingImages(String text) {
+        Image image = null;
+        try {
+            if(!"https:Brak".contains(text)) {
+                image = convertStrigInToImage(text);
+            } else {
+                text = "Brak obrazu";
+            }
+        } catch (IOException e) {
+            System.out.println("Blad przy ladowaniu obrazu");
+        }
+        return image;
+    }
+
 
 }
