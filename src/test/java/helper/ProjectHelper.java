871aabdaaeca5429259dcecc522769575700da35
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-28 10:24:50 +0100

    Added Scroll bar method

--- src/test/java/helper/ProjectHelper.java
+++ src/test/java/helper/ProjectHelper.java
@@ -1,7 +1,8 @@
 package helper;
 
-import org.openqa.selenium.JavascriptException;
+
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -13,8 +14,28 @@ public class ProjectHelper {
         return new Actions(driver);
     }
 
-    public static JavascriptExecutor js(WebDriver driver) {
+    public static JavascriptExecutor js (WebDriver driver) {
         return  (JavascriptExecutor) driver;
+    }
+
+    public static void sideBarMover(JavascriptExecutor js, int scrollAmount) throws InterruptedException {
+        int scrolls = 0;
+        while(true) {
+            try {
+                js.executeScript("window.scrollBy(0, " + scrollAmount + ");");
+                Thread.sleep(2000);
+                if(scrolls >= 3) {
+                    break;
+                }
+            } catch (NoSuchElementException e) {
+                throw e;
+            }
+            scrolls ++;
+        }
+    }
+
+    public static String addHttpsProtocolPrefix() {
+        return "https:";
     }
 
 }
