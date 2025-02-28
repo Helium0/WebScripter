4c1e5cf97a698d09f5e466ef17503dcd8c6bf020
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-28 10:25:41 +0100

    Implemented another methods for scrapping product details

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -1,19 +1,13 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import utilities.ProductComponent;
 import waits.ProjectWaits;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 
 public class ProductPage {
@@ -47,6 +41,8 @@ public class ProductPage {
     @FindBy(xpath = "//a[@title='Następna strona']")
     private WebElement nextPageElement;
 
+    @FindBy(xpath = ".//span[@class='ProductItem__Label Heading Text--subdued']")
+    private WebElement productTag;
 
 
     public ProductPage (WebDriver driver) {
@@ -91,14 +87,63 @@ public class ProductPage {
 
     }
 
-    public  String productPicture (WebElement element, By by, String text) {
+    public  String productPicture(WebElement element, By by, String text) {
         String productImage = "";
         try {
-            String pictureAttribute = element.findElement(by).getDomAttribute(text);
+            productImage= element.findElement(by).getDomAttribute(text);
         } catch (NoSuchElementException e) {
             productImage = "Brak obrazu";
         }
         return productImage;
     }
+
+    public boolean discountedProductOrNo(WebElement element, By by, JavascriptExecutor js) {
+        String updatedTagproduct = "";
+        boolean priceAfterdiscount = false;
+        try {
+            WebElement test = element.findElement(by);
+            updatedTagproduct = (String) js.executeScript("return arguments[0].textContent;", test);
+            if(updatedTagproduct.trim().contains("OFF")) {
+                priceAfterdiscount = true;
+                }
+
+            } catch (NoSuchElementException e) {
+
+        }
+        return priceAfterdiscount;
+
+    }
+
+    /* Te dwie metody discountedProductOrNo / productTagReturner moze wrzucic do Mapy? Klucz jako Cena i wartosc true albo false
+    tak samo z tagiem. Klucz Product Tag i wartość z metody
+    dodatkowo metoda productPriceReturner */
+
+    public String productTagReturner(WebElement element, By by, JavascriptExecutor js) {
+        String updatedTagproduct = "REGULAR";
+        try {
+            WebElement test = element.findElement(by);
+            updatedTagproduct = (String) js.executeScript("return arguments[0].textContent;", test);
+
+         } catch (NoSuchElementException e) {
+
+        }
+        return updatedTagproduct;
+    }
+
+    public String productPriceReturner(WebElement element, By by, JavascriptExecutor javascriptExecutor) {
+        String price = "";
+        WebElement prodP = element.findElement(by);
+        price = (String) javascriptExecutor.executeScript("return arguments[0].textContent;", prodP);
+        return price;
+    }
+
+    public String hyperLinkToTheProduct(List<WebElement> webElementList) {
+        String link = "";
+        for (WebElement element : webElementList) {
+            link = element.getDomAttribute("href");
+        }
+        return link;
+    }
+
 
 }
